18b0974a72a94b91b895981d539a9873876f1e23
José Luis González <jlgonzalezpisa@gmail.com>
2024-11-14 08:59:29 -0500

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,31 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Watch",
+            "Pants",
+            "Shirt",
+            "T.V.",
+            "Banana",
+            "Pots",
+            "Book",
+            "Ford",
+            "Chevy",
+            "Guitar",
+            "Wallet"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
