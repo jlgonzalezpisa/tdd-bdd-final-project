f6cbb85d32fc36594235f8e066264408b3c7ce6d
José Luis González <jlgonzalezpisa@gmail.com>
2024-11-19 08:46:35 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -207,6 +207,14 @@ class TestProductRoutes(TestCase):
         new_count = self.get_product_count()
         self.assertEqual(new_count, initial_count - 1)
 
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
+
     ######################################################################
     # Utility functions
     ######################################################################
